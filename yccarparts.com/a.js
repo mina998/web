563fd755308e6ea68a80842b87138079e8f78427
iosss <iosss@qq.com>
2023-06-28 23:12:02 +0800

    add

---
+++ yccarparts.com/a.js
@@ -0,0 +1,12 @@
+// window.addEventListener('scroll', () => {
+//     let __header_element = document.querySelector('#brx-header');
+//     let scroll_position = window.scrollY;
+//     console.log(scroll_position);
+//     if (scroll_position > 500) {
+//         if (!__header_element.classList.contains('up2')) {
+//             __header_element.classList.add('up2');
+//         }
+//     } else {
+//         __header_element.classList.remove('up2');
+//     }
+// });
